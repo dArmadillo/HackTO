347007e4a1a61d72a733bd5610260677741d34cb
Shameer Rehman <srehman@verto.ca>
2022-09-10 15:58:44 -0400

    fixing project card

--- frontend/src/components/project/ProjectCard.js
+++ frontend/src/components/project/ProjectCard.js
@@ -1,5 +1,7 @@
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
+import CardHeader from '@mui/material/CardHeader';
+
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
@@ -18,13 +20,17 @@ export default function ProjectCard() {
         <Typography sx={{ fontSize: 12 }} color="text.secondary" gutterBottom>
           Posted: August 31, 2022
         </Typography>
-        <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-        <Typography sx={{ mb: 1.5 }} color="text.primary">
-          Firstname Lastname
-        </Typography>
-        <Typography sx={{ mb: 1.0 }} color="text.secondary">
-          Job Title
-        </Typography>
+        <CardHeader
+          avatar={
+            <Avatar
+              alt="Remy Sharp"
+              src="/static/images/avatar/1.jpg"
+            />
+          }
+          title={"Firstname Lastname"}
+          subheader = {'Job Title'}
+        />
+      
         <Typography variant="body2">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip          
         </Typography>
