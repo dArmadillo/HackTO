cbd1560bdec18f42d6fe86537fb2fd12d7284fdc
Darren Liu <dliu@verto.ca>
2022-09-10 14:32:58 -0400

    feat: added association table for users and projects

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,28 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import enum
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/test.db"
 db = SQLAlchemy(app)
+
+# class UserProjectStates(enum.Enum):
+#     application = 1
+#     active = 2
+#     completed = 3
+
+user_project = db.Table(
+    "user_project",
+    db.Column("user_id", db.Integer, db.ForeignKey("user.id")),
+    db.Column("project_id", db.Integer, db.ForeignKey("project.id")),
+)
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    projects = db.relationship("Project", secondary=user_project, backref="users")
 
     def __repr__(self):
         return f"[USER-{self.id}] name: {self.username}"
--- backend/load_test_data.py
+++ backend/load_test_data.py
@@ -4,13 +4,24 @@ from app import User, Project
 db.drop_all()
 db.create_all()
 
-darren = User(username="darren", email="dar.liu1224@gmail.com")
 project1 = Project(
     title="Project 1",
     description="This is a project.",
     body="This the description of a project",
     creator="Amazon Inc.",
 )
-db.session.add(darren)
-db.session.add(project1)
+project2 = Project(
+    title="Project 2",
+    description="This is a project.",
+    body="This the description of a project",
+    creator="Facebook Inc.",
+)
+
+darren = User(username="darren", email="dar.liu1224@gmail.com")
+cheryl = User(username="cheryl", email="cheryl.chen.gura@gmail.com")
+
+project1.users.append(darren)
+project1.users.append(cheryl)
+db.session.add_all([project1, project2])
+db.session.add_all([darren, cheryl])
 db.session.commit()
